eb372077bec0f1287e2a9d8ac9e3b716e70e685b
MARCO SEBASTIAN GALVAN RAMIREZ <marco_sasori@hotmail.com>
2020-07-06 12:39:49 -0500

    project description page

---
+++ Assets/js/swiper.js
@@ -0,0 +1,25 @@
+ var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 20,
+          stretch: 0,
+          depth: 200,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      });
